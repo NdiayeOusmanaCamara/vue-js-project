131ce38c4f89fbd25c9e63121df25b91d47ed273
NdiayeOusmaneCamara <167888415+NdiayeOusmanaCamara@users.noreply.github.com>
2024-08-07 00:58:24 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+<!-- <h1>Hello World</h1> -->
+ <!-- <HelloWorld :sub-title="subTitle"/> -->
+ <!-- {{ subTitle }} -->
+   <!-- <HelloWorld :name="name" :age="age"/> -->
+   <!-- <HelloWorld :user="user"/>
+   <input type="text" v-model="user.name">
+   <input type="text" v-model="user.age"> -->
+   <HelloWorld :user="user"/>
+   <!-- <div 
+   v-if="user.age > 15">
+    Mon age
+
+   </div>
+   <div 
+   v-else >
+   Else
+
+   </div> -->
+   <input type="text" v-model="user.name" >
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue';
+// import { ref } from 'vue';
+// const subTitle = "lOrem ipsum dolor sit amet.";
+// const subTitle = ref ("Lorem ipsum dolor sit amet.")
+// const name = ref(value:"NordCoders");
+// const age = ref(value: 30);
+// const user = ref ( value: {
+//     name: 'NordCoders',
+//     age:30,
+// });
+// const display = () => {
+// console.log(name.value, age.value);
+// }
+// display()
+import { reactive } from 'vue';
+const user = reactive ({
+    name: 'NordCoders',
+    age:30,
+});
+const display = () =>{
+    console.log(user.name, user.age);
+}
+display()
+</script>
+
+
+
+<style scoped>
+
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+ <h1>Hello World</h1>
+ <!-- <p>Lorem ipsum dolor sit amet</p> -->
+  <!-- <p>{{ props.user.name}} {{ props.user.age}}</p> -->
+  <!-- <p>{{ props.subTitle}}</p> -->
+</template>
+
+<script setup>
+
+const props = defineProps(['user']);
+
+</script>
+
+
+
+<style scoped>
+
+
+</style>
